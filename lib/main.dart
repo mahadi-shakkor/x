02c8762d144b79d2532fb9efd150d116d9db58b4
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2025-07-30 23:55:42 +0600

    good

--- lib/main.dart
+++ lib/main.dart
@@ -30,28 +30,49 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List image = [
+    "images/0.png",
+    "images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+    "images/5.png",
+    "images/6.png",
+    "images/7.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.orangeAccent,
         body: Center(
-          child: ListView.separated(
-            itemCount: 10,
-            
+          child: SizedBox(
+            width: 300,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: image.length,
 
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                height: 200,
-                width: 100,
-                color: Colors.pinkAccent,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  height: 100,
 
-                child: Text('index $index'),
-              );
-            },
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox(height: 30);
-            },
+                  width: 100,
+
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(image[index]),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return SizedBox(height: 30);
+              },
+            ),
           ),
         ),
       ),
