d0ab4472f170c668fe1c2f6aeadb5d8ca1a8eeca
mahadi-shakkor <126345600+mahadi-shakkor@users.noreply.github.com>
2025-07-30 22:02:02 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,77 +30,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrimentCounter() {
-    if (_counter>0){
-      setState(() {
-        _counter--;
-      });
-
-
-
-    }
-
-  }
-  void _resetCounter() {
-
-    setState(() {
-      _counter=0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-
-      floatingActionButton: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          FloatingActionButton(
-            heroTag: 'btn1',
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            heroTag: 'btn3',
-            onPressed: _resetCounter,
-            tooltip: 'Reset',
-            child: Icon(Icons.reset_tv),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            heroTag: 'btn2',
-            onPressed: _decrimentCounter,
-            tooltip: 'Second Button',
-            child: Icon(Icons.remove),
-          ),
-        ],
-      ),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor :Colors.black,
+          body: Column(children: [
+        
+        
+        Text("data" ,style: TextStyle(color: Colors.red ,fontSize: 50),)
+      ])),
     );
   }
 }
